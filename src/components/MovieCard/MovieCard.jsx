8a75eff021b67e4c280e73051533666b6cffc681
mrazadtamal <mrazadt@gmail.com>
2021-12-26 17:20:33 +0600

    redux

---
+++ src/components/MovieCard/MovieCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./MovieCard.scss"
+
+const MovieCard = () => {
+    return (
+        <div>
+            
+            <h1>MovieCard</h1>
+        </div>
+    )
+}
+
+export default MovieCard
