b1d48811240f559300b0a8b8ebd7fe4a3bf85dfc
mrazadtamal <mrazadt@gmail.com>
2021-12-26 19:06:34 +0600

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,8 @@ import "./Header.scss"
 const Header = () => {
     return (
         <div className="header">
-      <Link to="/">
-        <div className="logo">MWBD</div>
+      <Link to="/" style={{ textDecoration: 'none' }}>
+        <div className="logo">MOVIE-WORLD-BD</div>
       </Link>
       <div className="user-image">
         <img src={"https://res.cloudinary.com/abidazad/image/upload/v1637747947/user_xsd3a7.png"} alt="user" />
