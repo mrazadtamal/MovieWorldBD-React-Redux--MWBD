539063216d89853a30acb1439dde38bf85eab235
mrazadtamal <mrazadt@gmail.com>
2021-12-27 00:20:35 +0600

    details

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className="logo">MOVIE-WORLD-BD</div>
       </Link>
       <div className="user-image">
-        <img src={"https://res.cloudinary.com/abidazad/image/upload/v1637747947/user_xsd3a7.png"} alt="user" />
+        <img src={"https://res.cloudinary.com/abidazad/image/upload/v1640542528/Vector_uslonh.png"} alt="user" />
       </div>
     </div>
     )
