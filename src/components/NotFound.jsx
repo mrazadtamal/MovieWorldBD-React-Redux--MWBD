0083175da8b350ebad3c2119a90ccdea6f5701fc
mrazadtamal <mrazadt@gmail.com>
2021-12-26 23:17:49 +0600

    card

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const Img = styled.img`
+width: 100%;
+height: 100vh
+`;
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>PageNotFound</h1>
-        </div>
+        <>
+        <Img src="https://res.cloudinary.com/abidazad/image/upload/v1636537146/2676383_ugnivd.jpg" alt="" />
+        </>
     )
 }
 
