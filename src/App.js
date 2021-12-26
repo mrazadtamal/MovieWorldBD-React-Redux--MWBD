d7ef6b5f9f5629f8834904cad9697d34c6471bb6
mrazadtamal <mrazadt@gmail.com>
2021-12-26 18:36:27 +0600

    api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import './App.scss';
-
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import PageNotFound from './components/PageNotFound';
+import MovieDetail from './components/MovieDetail/MovieDetail';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
+
   return (
-    <div className="App">
-     <h1>azad</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header></Header>
+        <div className="container">
+          <Routes>
+            <Route path="/"  element={<Home/>} />
+            <Route path="/movie/:imdbID" element={<MovieDetail/>} />
+            <Route element={<PageNotFound/>} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
 
