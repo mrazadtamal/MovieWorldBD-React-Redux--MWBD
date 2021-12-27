6e4deb9843a9088de353619da0a138c466012dc9
mrazadtamal <mrazadt@gmail.com>
2021-12-27 20:27:16 +0600

    90%

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import "./Header.scss"
 
 
 const Header = () => {
+const [movie, setMovie]=  useState("")
+ const submitHandlar = (e)=>{
+e.preventDifault()
+console.log(movie);
+ }
+
+
     return (
         <div className="header">
       <Link to="/" style={{ textDecoration: 'none' }}>
         <div className="logo">MOVIE-WORLD-BD</div>
       </Link>
+      <div className='search'>
+      <form onSubmit={submitHandlar}>
+        <input type="text" value={movie} onChange={(e)=>setMovie(e.target.value)} placeholder='Search' />
+        <button type='submit'>Search</button>
+      </form>
+      </div>
       <div className="user-image">
         <img src={"https://res.cloudinary.com/abidazad/image/upload/v1640542528/Vector_uslonh.png"} alt="user" />
       </div>
