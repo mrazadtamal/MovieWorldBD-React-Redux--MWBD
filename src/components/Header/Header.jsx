15dd3203dd7d88d178d11f0bb298a4cd9c70900f
mrazadtamal <mrazadt@gmail.com>
2021-12-28 10:40:48 +0600

    done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,36 +1,71 @@
-import React, { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { fetchAsyncMovies, fetchAsyncShows } from '../../redux/movies/movieSlice'
-import "./Header.scss"
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from "../../redux/movies/movieSlice";
+import "./Header.scss";
+import { FaSearchengin } from "react-icons/fa";
 
 const Header = () => {
-const [movie, setMovie]=  useState("")
-const dispatch =  useDispatch()
- const submitHandlar = (e)=>{
-      e.preventDefault()
-     if(movie === "") return alert("Please Enter a Movie Name")
-      dispatch(fetchAsyncMovies(movie))
-      dispatch(fetchAsyncShows(movie))
-      setMovie("")
- }
-   return (
-        <div className="header">
-      <Link to="/" style={{ textDecoration: 'none' }}>
-        <div className="logo">MOVIE-WORLD-BD</div>
-      </Link>
-      <div className='search'>
-      <form onSubmit={submitHandlar}>
-        <input type="text" value={movie} onChange={(e)=>setMovie(e.target.value)} placeholder='Search' />
-        <button type='submit'>Search</button>
-      </form>
+  const [movie, setMovie] = useState("");
+  const [active, setActive] = useState(false);
+  const dispatch = useDispatch();
+  const submitHandlar = (e) => {
+    e.preventDefault();
+    if (movie === "") return alert("Please Enter a Movie Name");
+    dispatch(fetchAsyncMovies(movie));
+    dispatch(fetchAsyncShows(movie));
+    setMovie("");
+  };
+  return (
+    <div>
+      <div className="header">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <div className="logo desktop">MOVIE-WORLD-BD</div>
+          <div className="logo mobile">MWB</div>
+        </Link>
+        <div className="search desktop">
+          <form onSubmit={submitHandlar}>
+            <input
+              type="text"
+              value={movie}
+              onChange={(e) => setMovie(e.target.value)}
+              placeholder="Search"
+            />
+            <button type="submit">Search</button>
+          </form>
+        </div>
+
+        <div className="user-image">
+          <img
+            src={
+              "https://res.cloudinary.com/abidazad/image/upload/v1640542528/Vector_uslonh.png"
+            }
+            alt="user"
+          />
+        </div>
+
+        <div className="search mobile">
+          <button onClick={() => setActive((prev) => !prev)}>
+            <FaSearchengin />
+          </button>
+        </div>
       </div>
-      <div className="user-image">
-        <img src={"https://res.cloudinary.com/abidazad/image/upload/v1640542528/Vector_uslonh.png"} alt="user" />
+      <div className={!active ? "search-m active" : "search-m"}>
+        <form onSubmit={submitHandlar}>
+          <input
+            type="text"
+            value={movie}
+            onChange={(e) => setMovie(e.target.value)}
+            placeholder="Search"
+          />
+          <button type="submit">Search</button>
+        </form>
       </div>
     </div>
-    )
-}
+  );
+};
 
-export default Header
+export default Header;
